42833ac988b979666da5aa20cf576ae60473347d
spences10 <spences10apps@gmail.com>
2024-01-23 22:06:11 +0000

    refactor: :recycle: add buttons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<button class="btn">neutral</button>
+<button class="btn btn-primary">primary</button>
+<button class="btn btn-success">sucess</button>
+<button class="btn btn-warning">warning</button>
+<button class="btn btn-error">error</button>
